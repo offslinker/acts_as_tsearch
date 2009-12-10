ef7bd6581654777c1e0fa5a331efcaf19e11d958
Wolfram Arnold <wolfram@rubyfocus.biz>
2009-12-10 11:27:23 -0800

    Fix class nesting issue which caused blank string defaults to not show up as such. See also: https://rails.lighthouseapp.com/projects/8994/tickets/2683-postgres-invalid-default-value-getting-from-character-varying-columns

--- lib/postgres_extensions.rb
+++ lib/postgres_extensions.rb
@@ -14,7 +14,7 @@ module ActiveRecord
         )
       end
       
-    
+    end
     class PostgreSQLColumn < Column #:nodoc:
       private
         alias_method :simplified_type_base, :simplified_type
@@ -30,8 +30,6 @@ module ActiveRecord
               simplified_type_base(field_type)
           end
         end
-    end
-
     end
 
   end
